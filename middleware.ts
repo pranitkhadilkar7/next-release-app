1f04bc506317e6cc6463eff5021e044d908cf430
Pranit Khadilkar <pranit@emtech.com>
2024-07-07 14:13:11 +0530

    added middleware check for authrization

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,26 @@
 import { NextRequest, NextResponse } from 'next/server'
+import { TOKEN } from './common/constants/cookie-keys'
 
 export function middleware(request: NextRequest) {
-  console.log(request)
-  return NextResponse.redirect('/')
+  const token = request.cookies.get(TOKEN)?.value
+
+  if (
+    request.nextUrl.pathname.startsWith('/login') ||
+    request.nextUrl.pathname.startsWith('/signup')
+  ) {
+    if (token) {
+      return NextResponse.redirect(new URL('/', request.url))
+    }
+  }
+
+  if (request.nextUrl.pathname.startsWith('/create-release')) {
+    if (!token) {
+      return NextResponse.redirect(new URL('/', request.url))
+    }
+  }
+  console.log(token)
 }
 
 export const config = {
-  matcher: '/release/:path*',
+  matcher: ['/login', '/signup', '/create-release'],
 }
